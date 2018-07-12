0a4122b4ed2dfbdd91a907498950df6be91c92d2
Carl Hain <crhain@gmail.com>
2018-07-12 17:34:26 -0500

    Add basic game loop to Main

--- ConsoleRPG/Program.cs
+++ ConsoleRPG/Program.cs
@@ -6,8 +6,28 @@ namespace ConsoleRPG
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            Console.WriteLine("Type 'q' to quit");
+            Console.WriteLine("Type 'h' for help");
+            Console.WriteLine("");
+            while (true)
+            {
+                Console.Write("What shall we do? : ");
+                string input = Console.ReadLine();
+
+                if(input == "q")
+                {
+                    break;
+                }
+
+                if(input == "h")
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Type 'q' to quit");
+                    Console.WriteLine("Type 'h' for help");
+                    Console.WriteLine("");
+                }
+            }
+            
         }
     }
 }
