69ec15df07c55bebe3e2272af9ec1a8bca445b6e
Carl Hain <crhain@gmail.com>
2018-07-12 19:58:00 -0500

    Add scaffolding for unit tests using xunit

---
+++ ConsoleRPG/Core/Test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleRPG.Core
+{
+    class Test
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ ConsoleRPG/Tests/Tests.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleRPG.Core;
+using Xunit;
+
+namespace ConsoleRPG.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void TestTest()
+        {
+            var test = new Test()
+            {
+                Name = "Bob"
+            };
+
+            Assert.Equal("Bob", test.Name);
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
