cc7192942ab103e1498e0760c854e069014b1879
Carl Hain <crhain@gmail.com>
2018-07-13 18:32:58 -0500

    Fix namespace on UserInterfaceTest.cs and ScreenTest.cs; add failing tests stubs to them

--- ConsoleRPG/Tests/UI/ScreenTest.cs
+++ ConsoleRPG/Tests/UI/ScreenTest.cs
@@ -1,30 +1,15 @@
 ﻿using ConsoleRPG.Core;
 using Xunit;
 
-namespace ConsoleRPG.Tests
+namespace ConsoleRPG.Tests.UI
 {
     public class ScreenTest
     {
         [Fact]
         public void TestTest()
-        {
-            var test = new Test()
-            {
-                Name = "Bob"
-            };
-
-            Assert.Equal("Bob", test.Name);
+        {           
+            Assert.True(false);
         }
-
-        [Fact]
-        public void FailingTest()
-        {
-            Assert.Equal(5, Add(2, 2));
-        }
-
-        int Add(int x, int y)
-        {
-            return x + y;
-        }
+       
     }
 }
