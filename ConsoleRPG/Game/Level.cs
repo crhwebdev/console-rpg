34ef9be74b07e1aede77781911ee4a5398e01dd5
Carl Hain <crhain@gmail.com>
2018-07-21 19:36:21 -0500

    Add simple TestDungeon with two rooms.

--- ConsoleRPG/Game/Level.cs
+++ ConsoleRPG/Game/Level.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleRPG.Game.Locations;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,7 @@ namespace ConsoleRPG.Game
 {
     public abstract class Level
     {
+        public virtual List<Location> Locations { get; set; }
+        
     }
 }
---
+++ ConsoleRPG/Game/TestDungeon.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleRPG.Game.Locations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleRPG.Game
+{
+    class TestDungeon : Level
+    {
+        public override List<Location> Locations { get; set; }
+        
+        public TestDungeon()
+        {
+            var location = new Area("The Chamber")
+            {
+                Description = "a dark dank chamber full of soft whispering voices... there is a small ornate door in the north wall."
+            };
+            var location2 = new Area("The Antechamber")
+            {
+                Description = "a small, brightly lit room with a small ornate door leading to the south."
+            };
+
+            location.ExitNorth = location2;
+            location2.ExitSouth = location;
+
+            Locations.Add(location);
+            Locations.Add(location2);
+        }
+    }
+}
