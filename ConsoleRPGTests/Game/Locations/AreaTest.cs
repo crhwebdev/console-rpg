bde35ec746b0accfd7448b18189c89065fcc2fee
Carl Hain <crhain@gmail.com>
2018-07-24 08:52:13 -0500

    Write Area tests and get them to pass

--- ConsoleRPGTests/Game/Locations/AreaTest.cs
+++ ConsoleRPGTests/Game/Locations/AreaTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ConsoleRPG.Game.Actors;
+using ConsoleRPG.Game.Locations;
+using System;
 using System.Linq;
 using Xunit;
 
@@ -6,22 +8,44 @@ namespace ConsoleRPGTests.Game.Locations
 {
     public class AreaTest
     {
+        private string _areaName;
+        private string _areaDescription;
+        private Area _area;
+        private Player _player;
+
+        public AreaTest()
+        {
+            _player = new Player("Testy Tess");
+            _areaName = "Testing Ground";
+            _areaDescription = "Test your mettle in the testing ground!";
+            _area = new Area(_areaName)
+            {
+                Description = _areaDescription
+            };
+            
+        }
+
         [Fact]
         public void CanInstantiateAreaWithAName()
-        {
-            Assert.True(false);
+        {            
+            Assert.Equal(_areaName, _area.Name);
         }
 
         [Fact]
         public void AreaEnterMethodReturnsAppropriateTextAndSetsPlayerLocation()
         {
-            Assert.True(false);
+            var areaEnterResult = _area.Enter(_player).ToString();
+
+            Assert.Equal(_player.Location, _area);
+            Assert.Equal("You enter " + _areaDescription, areaEnterResult);
         }
 
         [Fact]
         public void AreaViewMethodReturnsAppropriateText()
         {
-            Assert.True(false);
+            var areaViewResult = _area.Viewed(_player).ToString();
+
+            Assert.Equal("You see " + _areaDescription, areaViewResult);
         }
 
        
