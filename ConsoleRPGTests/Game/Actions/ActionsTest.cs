960f1eead9ab6fd897e09084baa9d2df34b4b2b2
Carl Hain <crhain@gmail.com>
2018-07-25 09:36:59 -0500

    Move Action tests to one test file: ActionsTest.cs

---
+++ ConsoleRPGTests/Game/Actions/ActionsTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace ConsoleRPGTests.Game.Actions
+{
+    public class ActionsTest
+    {
+        [Fact]
+        public void LookActionReturnsCorrectDisplayText()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void MessageActionReturnsCorrectDisplayText()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void MoveActionReturnsCorrectDisplayText()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void QuitActionReturnsCorrectDisplayText()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void SayActionReturnsCorrectDisplayText()
+        {
+            Assert.True(false);
+        }
+    }
+}
