f1e5025948654fa8de05d84f5352e6292f60fc97
Carl Hain <crhain@gmail.com>
2018-07-27 11:43:33 -0500

    Add Prop Abstract class.

---
+++ ConsoleRPG/Game/Props/Prop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConsoleRPG.Game.Actors;
+using ConsoleRPG.Game.Locations;
+using ConsoleRPG.System;
+
+namespace ConsoleRPG.Game.Props
+{
+    public abstract class Prop : IViewable
+    {
+        public virtual string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public virtual string Description { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public virtual Location Location { get; set; }
+
+        public abstract DisplayText Viewed(Actor viewer);
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+    }
+}
---
+++ ConsoleRPGTests/Game/Actors/NPCTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace ConsoleRPGTests.Game.Actors
+{
+    public class NPCTest
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            Assert.True(false);
+        }
+    }
+}
