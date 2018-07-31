3d4ba9543aaca942504aec0380704c80101e3d15
Carl Hain <crhain@gmail.com>
2018-07-31 13:57:55 -0500

    Add stubs for UtilTest.

---
+++ ConsoleRPGTests/System/UtilTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace ConsoleRPGTests.System
+{
+    public class UtilTest
+    {
+        [Fact]
+        public void GetViewableMatchInLocationReturnsCorrectMatch()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void GetActorMatchInLocationnReturnsCorrectMatch()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void GetItemMatchInLocationReturnsCorrectMatch()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void GetExitMatchInLocationReturnsCorrectMatch()
+        {
+            Assert.True(false);
+        }
+    }
+}
