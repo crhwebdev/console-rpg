9731e66d54f5e26cbc9e4f12356cd75eed20299d
Carl Hain <crhain@gmail.com>
2018-07-31 14:16:17 -0500

    Add GetItemMatchInLocationReturnsCorrectMatch to UtilTest and get to run.

--- ConsoleRPGTests/System/UtilTest.cs
+++ ConsoleRPGTests/System/UtilTest.cs
@@ -35,13 +35,32 @@ namespace ConsoleRPGTests.System
         [Fact]
         public void GetActorMatchInLocationnReturnsCorrectMatch()
         {
-            Assert.True(false);
+            var locationName = "Testing Area";
+            var npcName = "Test Dude";
+
+            var location = new Location(locationName);
+            var viewNPC = new NPC(npcName);
+            location.Actors.Add(viewNPC);
+
+            Assert.Null(Util.GetActorMatchInLocation(location, "Wrong Dude"));
+            Assert.Equal(npcName, Util.GetActorMatchInLocation(location, npcName).Name);
+            Assert.Equal(npcName, Util.GetActorMatchInLocation(location, npcName.ToLower()).Name);
+            
         }
 
         [Fact]
         public void GetItemMatchInLocationReturnsCorrectMatch()
         {
-            Assert.True(false);
+            var locationName = "Testing Area";
+            var itemName = "Test Item";
+
+            var location = new Location(locationName);
+            var viewItem = new Item(itemName);
+            location.Items.Add(viewItem);
+
+            Assert.Null(Util.GetItemMatchInLocation(location, "Wrong Thing"));
+            Assert.Equal(itemName, Util.GetItemMatchInLocation(location, itemName).Name);
+            Assert.Equal(itemName, Util.GetItemMatchInLocation(location, itemName.ToLower()).Name);
         }
 
         [Fact]
