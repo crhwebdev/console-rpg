4c17cc003dd7230ce52dbc55df883330d455d854
Carl Hain <crhain@gmail.com>
2018-08-15 11:58:24 -0500

    Add Armor class that inherits from Item and IEquipable.

--- ConsoleRPG/Game/Props/Armor.cs
+++ ConsoleRPG/Game/Props/Armor.cs
@@ -5,16 +5,14 @@ using System.Text;
 
 namespace ConsoleRPG.Game.Props
 {
-    public class Weapon : Item, IEquipable
+    public class Armor : Item, IEquipable
     {
-
-        public Weapon(string name, int attackBonus = 0) : base(name)
+        public Armor(string name, int defenseBonus = 0) : base(name)
         {
             //for now, we set slot to MainWeapon.
-            EquipableSlot = EquipmentSlots.MainWeapon;
-            AttackBonus = attackBonus;
-            DefenseBonus = 0;
-            
+            EquipableSlot = EquipmentSlots.Body;
+            DefenseBonus = defenseBonus;
+            AttackBonus = 0;
         }
 
         public EquipmentSlots EquipableSlot { get; set; }
