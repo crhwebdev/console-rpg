f058ec2485b244ffb0287e7653d2b0363949bd92
Carl Hain <crhain@gmail.com>
2018-09-01 15:53:44 -0500

    Do a bit of reorganizing to DisplayText class and add comments.

--- ConsoleRPG/System/DisplayText.cs
+++ ConsoleRPG/System/DisplayText.cs
@@ -9,18 +9,19 @@ namespace ConsoleRPG.System
 {
     public class DisplayText : IEnumerable
     {
-        private List<DisplayTextLine> _content;
-        public int LineCount => _content.Count;
 
-        //Constructor methods that intializes _content with different types
+        ////////////////////////////////////////////////////////////////////////////////////////
+        //   CONSTRUCTORS
+        //////////////////////////////////////////////////////////////////////////////////////// 
+        
         public DisplayText()
         {
             _content = new List<DisplayTextLine>();
         }
-
+      
         public DisplayText(string message)
         {
-            _content = new List<DisplayTextLine> { new DisplayTextLine(message) };            
+            _content = new List<DisplayTextLine> { new DisplayTextLine(message) };
         }
 
         public DisplayText(DisplayTextLine line)
@@ -33,22 +34,54 @@ namespace ConsoleRPG.System
             _content = messages;
         }
 
-        //Various Add methods for different types
+        ////////////////////////////////////////////////////////////////////////////////////////
+        //   PUBLIC PROPS                          
+        //////////////////////////////////////////////////////////////////////////////////////// 
+
+        public int LineCount => _content.Count;
+
+        ////////////////////////////////////////////////////////////////////////////////////////
+        //   PRIVATE FIELDS                          
+        //////////////////////////////////////////////////////////////////////////////////////// 
+
+        private List<DisplayTextLine> _content;
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////
+        //   PUBLIC METHODS                          
+        //////////////////////////////////////////////////////////////////////////////////////// 
+        
+        /// <summary>
+        /// Add string text
+        /// </summary>
+        /// <param name="line"></param>
         public void Add(string line)
         {            
             Add(new DisplayTextLine(line));
         }
 
+        /// <summary>
+        /// Add DisplayTextLine
+        /// </summary>
+        /// <param name="line"></param>
         public void Add(DisplayTextLine line)
         {
             _content.Add(line);
         }
 
+        /// <summary>
+        /// Add List of DisplayTextLine
+        /// </summary>
+        /// <param name="lines"></param>
         public void Add(List<DisplayTextLine> lines)
         {
             _content.AddRange(lines);
         }
 
+        /// <summary>
+        /// Concatenate DisplayText (can also use overloaded "+" operator)
+        /// </summary>
+        /// <param name="displayText"></param>
         public void Add(DisplayText displayText)
         {
             foreach(var line in displayText)
@@ -56,7 +89,11 @@ namespace ConsoleRPG.System
                 _content.Add((DisplayTextLine)line);
             }
         }
-            
+
+        ////////////////////////////////////////////////////////////////////////////////////////
+        //   PUBLIC OVERRIDE METHODS FROM BASE                          
+        //////////////////////////////////////////////////////////////////////////////////////// 
+
         public override string ToString()
         {
             var displayTextAsString = "";
@@ -78,11 +115,22 @@ namespace ConsoleRPG.System
                 }
             }
             
-
             return displayTextAsString; 
         }
 
-        //Implementation for the GetEnumerator method
+        
+        public static DisplayText operator +(DisplayText d1, DisplayText d2)
+        {
+            d1.Add(d2);
+
+            return d1;
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////
+        //   IENUMERATOR IMPLIMENTATION                          
+        //////////////////////////////////////////////////////////////////////////////////////// 
+
+
         IEnumerator IEnumerable.GetEnumerator()
         {
 
@@ -97,63 +145,6 @@ namespace ConsoleRPG.System
             {
                 yield return line;
             }
-        }
-
-        //overload + operator
-
-        public static DisplayText operator +(DisplayText d1, DisplayText d2)
-        {
-            d1.Add(d2);
-
-            return d1;
-        }
-                               
-    }
-
-    //public class DisplayTextEnum : IEnumerator
-    //{
-    //    public List<DisplayTextLine> Content;
-
-    //    int position = -1;
-
-    //    public DisplayTextEnum(List<DisplayTextLine> lines)
-    //    {
-    //        Content = lines;
-    //    }
-
-    //    public bool MoveNext()
-    //    {
-    //        position++;
-    //        return (position < Content.Count);
-    //    }
-
-    //    public void Reset()
-    //    {
-    //        position = -1;
-    //    }
-
-    //    object IEnumerator.Current
-    //    {
-    //        get
-    //        {
-    //            return Current;
-    //        }
-    //    }
-
-    //    public DisplayTextLine Current
-    //    {
-    //        get
-    //        {
-    //            try
-    //            {
-    //                return Content[position];
-    //            }
-    //            catch (IndexOutOfRangeException)
-    //            {
-    //                throw new InvalidOperationException();
-    //            }
-    //        }
-    //    }
-    //}
-
+        }                                       
+    }    
 }
