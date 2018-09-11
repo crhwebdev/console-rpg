84b375cd8ea55d31517fe5cff16f274344ece0c5
Carl Hain <crhain@gmail.com>
2018-09-11 12:21:38 -0500

    Reorganize Unequip class and add comments.

--- ConsoleRPG/Game/Actions/Unequip.cs
+++ ConsoleRPG/Game/Actions/Unequip.cs
@@ -8,17 +8,36 @@ using System.Text;
 namespace ConsoleRPG.Game.Actions
 {
     public class Unequip : Action
-    {       
-        private Actor _actor;
-        private string _target;
+    {
 
-        //needs access to player (i.e. actor), portal (i.e. reciever), and then other stuff depending on Action
+        ////////////////////////////////////////////////////////////////////////////////////////
+        //   CONSTRUCTOR                          
+        ////////////////////////////////////////////////////////////////////////////////////////
+
         public Unequip(Actor actor, string target)
         {
             _actor = actor;
             _target = target;
         }
 
+
+        ////////////////////////////////////////////////////////////////////////////////////////
+        //   PRIVATE FIELDS                          
+        ////////////////////////////////////////////////////////////////////////////////////////
+
+        private Actor _actor;
+        private string _target;
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////
+        //   PUBLIC METHODS                          
+        //////////////////////////////////////////////////////////////////////////////////////// 
+
+        /// <summary>
+        /// Executes Unequip action, which calls 
+        ///  Unequip method on Actor.
+        /// </summary>
+        /// <returns>DisplayText</returns>
         public override DisplayText Do()
         {                                     
             return _actor.Unequip(_target); 
