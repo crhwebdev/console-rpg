4fcdade03b015dc85b3fc97ba26b5c6936df3452
Carl Hain <crhain@gmail.com>
2018-09-17 13:48:13 -0500

    Reorganize Prop abstract class and add comments.

--- ConsoleRPG/Game/Props/Prop.cs
+++ ConsoleRPG/Game/Props/Prop.cs
@@ -10,15 +10,34 @@ namespace ConsoleRPG.Game.Props
 {
     public abstract class Prop : IViewable
     {
-        public virtual string Name { get; set; }
-        public virtual string Description { get; set; }
-        public virtual Location Location { get; set; }
+
+        ////////////////////////////////////////////////////////////////////////////////////////
+        //   PUBLIC CONSTRUCTOR                          
+        ////////////////////////////////////////////////////////////////////////////////////////
 
         public Prop(string name)
         {
             Name = name;
         }
 
+        ////////////////////////////////////////////////////////////////////////////////////////
+        //   PUBLIC PROPS                          
+        //////////////////////////////////////////////////////////////////////////////////////// 
+
+        public virtual string Name { get; set; }
+        public virtual string Description { get; set; }
+        public virtual Location Location { get; set; }
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////
+        //   PUBLIC METHODS                          
+        //////////////////////////////////////////////////////////////////////////////////////// 
+
+        /// <summary>
+        /// Returns a DisplayText object with the text description of the Prop
+        /// </summary>
+        /// <param name="viewer">The actor object that is calling this method and to whcih a DisplayText object is returned</param>
+        /// <returns>A DisplayText object</returns>
         public virtual DisplayText Viewed(Actor viewer)
         {
             return new DisplayText(Description);
