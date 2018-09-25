649cc26380a4d74756d27e1d46f8aea54b18f724
Carl Hain <crhain@gmail.com>
2018-09-24 19:24:43 -0500

    Add comments to ConsoleWrapper class

--- ConsoleRPG/System/ConsoleWrapper.cs
+++ ConsoleRPG/System/ConsoleWrapper.cs
@@ -6,16 +6,32 @@ namespace ConsoleRPG.System
 {
     public class ConsoleWrapper : IConsole
     {
+        ////////////////////////////////////////////////////////////////////////////////////////
+        //   PUBLIC METHODS                          
+        //////////////////////////////////////////////////////////////////////////////////////// 
+
+        /// <summary>
+        /// Wrapper for Console.Write
+        /// </summary>
+        /// <param name="text"></param>
         public void Write(string text)
         {
             Console.Write(text);
         }
 
+        /// <summary>
+        /// Wrapper for Console.WriteLine
+        /// </summary>
+        /// <param name="text"></param>
         public void WriteLine(string text)
         {
             Console.WriteLine(text);
         }
 
+        /// <summary>
+        /// Wrapper for Console.Readline
+        /// </summary>
+        /// <returns></returns>
         public string ReadLine()
         {
             return Console.ReadLine();
