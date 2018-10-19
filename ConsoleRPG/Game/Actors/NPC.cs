267177029e8b526cd167ad1588c07a1c2fee133e
Carl Hain <crhain@gmail.com>
2018-10-19 15:49:41 -0500

    Move methods into NPC class and out of namespace!

--- ConsoleRPG/Game/Actors/NPC.cs
+++ ConsoleRPG/Game/Actors/NPC.cs
@@ -13,20 +13,22 @@ namespace ConsoleRPG.Game.Actors
 
     public class NPC : Actor
     {
-        public NPC(string name) : base(name) {}                                       
+        public NPC(string name) : base(name) {}
+
+        ////////////////////////////////////////////////////////////////////////////////////////
+        //   PUBLIC METHODS                          
+        ////////////////////////////////////////////////////////////////////////////////////////        
+
+        /// <summary>
+        /// Handles Attack command for NPC
+        /// </summary>
+        /// <param name="commandClauseString"></param>
+        /// <returns>DisplayText of results of attack</returns>
+        public override DisplayText Attack(string commandClauseString)
+        {
+            return new DisplayText(Name + " attacks!");
+        }
     }
 
-    ////////////////////////////////////////////////////////////////////////////////////////
-    //   PUBLIC METHODS                          
-    ////////////////////////////////////////////////////////////////////////////////////////        
-
-    /// <summary>
-    /// Handles Attack command for NPC
-    /// </summary>
-    /// <param name="commandClauseString"></param>
-    /// <returns>DisplayText of results of attack</returns>
-    public override DisplayText Attack(string commandClauseString)
-    {
-        return new DisplayText(Name + " attacks!");
-    }
+  
 }
