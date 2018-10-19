1ba05a3c1efad38dfd960e7aa91148ba7a1677be
Carl Hain <crhain@gmail.com>
2018-10-19 15:32:34 -0500

    Add Attack action

---
+++ ConsoleRPG/Game/Actions/Attack.cs
@@ -0,0 +1,39 @@
+﻿using ConsoleRPG.Game.Actors;
+using ConsoleRPG.System;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleRPG.Game.Actions
+{
+    public class Attack : Action
+    {
+
+        public Attack(Actor actor, string commandClauseString)
+        {
+            _actor = actor;
+            _target = commandClauseString;
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////
+        //   PRIVATE FIELDS                          
+        //////////////////////////////////////////////////////////////////////////////////////// 
+
+        private Actor _actor;
+        private string _target;
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////
+        //   PUBLIC METHODS                          
+        //////////////////////////////////////////////////////////////////////////////////////// 
+
+        /// <summary>
+        /// Executes Attack action on actor 
+        /// </summary>
+        /// <returns>DisplayText</returns>
+        public override DisplayText Do()
+        {
+            return _actor.Attack(_target);
+        }
+    }
+}
