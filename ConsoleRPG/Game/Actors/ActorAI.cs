1e930f76d86d26cb731633c175ddde8aa23585b4
Carl Hain <crhain@gmail.com>
2018-11-04 12:11:44 -0600

    Change CombatManager class to ActorAI class; ActorAI class impliments IActorAI inteface with stub for DetermineCombatRound method

--- ConsoleRPG/Game/Actors/ActorAI.cs
+++ ConsoleRPG/Game/Actors/ActorAI.cs
@@ -1,64 +1,52 @@
-﻿using System;
+﻿using ConsoleRPG.Game.Actors;
+using ConsoleRPG.Game.Interfaces;
+using ConsoleRPG.System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ConsoleRPG.Game
 {
-    public class CombatManager
+    public class ActorAI : IActorAI
     {
 
         ////////////////////////////////////////////////////////////////////////////////////////
         //   PROTECTED CONSTRUCTOR  - Is a Singelton                        
         //////////////////////////////////////////////////////////////////////////////////////// 
 
-        protected CombatManager(GameEngine game)
-        {
-            _game = game;
-        }
+        protected ActorAI(){}
 
         ////////////////////////////////////////////////////////////////////////////////////////
         //   PRIVATE FIELDS                          
         //////////////////////////////////////////////////////////////////////////////////////// 
 
-        private static CombatManager _instance = null;
-        private GameEngine _game = null;
-
+        private static ActorAI _instance = null;        
 
         ////////////////////////////////////////////////////////////////////////////////////////
         //   PUBLIC METHODS                          
         ////////////////////////////////////////////////////////////////////////////////////////
         
         /// <summary>
-        /// This method returns either the existing Instance of CombatManager or creates a new one
+        /// This method returns either the existing Instance of ActorAI or creates a new one
         /// a reference to the GameEngine must be passed in. 
         /// </summary>
         /// <param name="game">reference to GameEngine</param>
         /// <returns>Instance of CombatManager</returns>
-        public static CombatManager Instance(GameEngine game)
+        public static ActorAI Instance()
         {
             if (_instance == null)
             {
-                _instance = new CombatManager(game);
+                _instance = new ActorAI();
             }
 
             return _instance;
         }
 
-
-        /// <summary>
-        /// This returns an reference to the existing CombatManager instance without requiring 
-        /// that a reference to an existing GameEngine be passed in. If it is called before their
-        /// is an instance, it will return an error.
-        /// </summary>
-        /// <returns>Instance of CombatManager</returns>
-        public static CombatManager Instance()
+        public DisplayText DetermineCombatRound(Actor target)
         {
-            if(_instance == null)
-            {
-                throw new Exception("CombatManager not initialzied");
-            }
-
-            return _instance;
+            return new DisplayText("DetermineCombatRound called!");
         }
+
+        
     }
 }
