561f52d1987feb88487dfc139e30791422575745
Carl Hain <crhain@gmail.com>
2018-11-04 12:02:58 -0600

    Create IActorAI interface to be used in implementing stragegy pattern for ActorAI

---
+++ ConsoleRPG/Game/Interfaces/IActorAI.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleRPG.Game.Actors;
+using ConsoleRPG.System;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleRPG.Game.Interfaces
+{
+    public interface IActorAI
+    {
+        DisplayText DetermineCombatRound(Actor target);
+        
+    }
+}
